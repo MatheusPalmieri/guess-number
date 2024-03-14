bf177ce2c14b214f99c64005326389c74762d52f
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-03-14 00:15:27 -0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Game: Guess the number.\n");
+    let mut guesses: u32 = 0;
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Enter a number: ");
+        let mut guess: String = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        guesses += 1;
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("\nYou win!");
+                break;
+            },
+            Ordering::Greater => println!("\nToo low!"),
+            Ordering::Less => println!("\nToo high!"),
+        }
+
+        println!("You've used {} attempts!", guesses);
+        println!("\n------------------\n")
+    }
+}
